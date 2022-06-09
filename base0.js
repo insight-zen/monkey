d5261cbaca9ac46a6d81f7d01af439de700e4092
mu09_m1 <mu09@mbam1.local>
2022-06-09 13:21:58 -0400

    base0 cleanup, mutation observer test  * base0 includes module C for cookies  * mutation observer based code for wapo

--- base0.js
+++ base0.js
@@ -4,12 +4,90 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://*/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      -
-// @description 6/2/2022, 9:07:42 AM
+// @description Jun 10 2022, 9:07:42 AM Added C, qsa extended
 // ==/UserScript==
 
+const C = {
+  list(){
+    const rv = document.cookie.split(";").map(e => e.split("=")).map(e => e[0] ).sort()
+    return rv
+  },
+
+  // returns the cookie with the given name,
+  // or undefined if not found
+  get(name) {
+    let matches = document.cookie.match(new RegExp(
+      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  },
+
+  set(name, value, options = {}) {
+    options = {
+      path: '/',
+      // add other defaults here if necessary
+      ...options
+    };
+
+    if (options.expires instanceof Date) {
+      options.expires = options.expires.toUTCString();
+    }
+
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+    for (let optionKey in options) {
+      updatedCookie += "; " + optionKey;
+      let optionValue = options[optionKey];
+      if (optionValue !== true) {
+        updatedCookie += "=" + optionValue;
+      }
+    }
+    document.cookie = updatedCookie;
+  },
+
+  delete(name) {
+    this.set(name, "", {
+      'max-age': -1
+    })
+  },
+
+  // https://stackoverflow.com/questions/179355/clearing-all-cookies-with-javascript#
+  clear(){
+    var cookies = document.cookie.split("; ");
+    for (var c = 0; c < cookies.length; c++) {
+        var d = window.location.hostname.split(".");
+        while (d.length > 0) {
+            var cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
+            var p = location.pathname.split('/');
+            document.cookie = cookieBase + '/';
+            while (p.length > 0) {
+                document.cookie = cookieBase + p.join('/');
+                p.pop();
+            };
+            d.shift();
+        }
+    }
+  },
+
+  clear2(){
+    document.cookie.replace(/(?<=^|;).+?(?=\=|;|$)/g, name => location.hostname.split('.').reverse().reduce(domain => (domain=domain.replace(/^\.?[^.]+/, ''),document.cookie=`${name}=;max-age=0;path=/;domain=${domain}`,domain), location.hostname));
+  }
+}
+
 const D = {
+  c: C,
+  h: document.querySelector("html"),
+  b: document.querySelector("body"),
+
+  purge(){
+    localStorage.clear()
+    sessionStorage.clear()
+    this.c.clear2()
+    this.c.clear()
+  },
+
   // https://javascript.info/blob
   // makeDownload(fileName: "a.txt", content: "Hello Dude")
   makeDownload(opts={}){
@@ -27,15 +105,36 @@ const D = {
     a.href = URL.createObjectURL(blob)
     a.download = opts.fileName || `#{demo}#{fileExt}`
     a.click();
-    URL.revokeObjectURL(a.href);
+    URL.revokeObjectURL(a.href)
   },
 
-  qs(pattern, parent=document){
+  // https://github.com/WebDevSimplified/js-util-functions/blob/main/domUtils/domUtils.js
+  qs(pattern, {parent = document, ...opts} = {}){
     return parent.querySelector(pattern)
   },
 
-  qsa(pattern, parent=document){
-    return [...parent.querySelectorAll(pattern)]
+  // filter: provide a function that takes in the node element and return true/false
+  //   f = (e) => e.id.match(/^(pay|soft)wall/)
+  //   qsa("div", {filter: f }) => qsa("div").filter(e => e.id.match(/^(pay|soft)wall/) )
+  // delete: boolean argument. If provided, will delete all matching elements
+  //   qsa("div", {delete: true})  => qsa("div").forEach(e => e.remove() )
+  qsa(pattern, {parent = document, ...opts} = {}) {
+    let rv = [...parent.querySelectorAll(pattern)]
+    if (Object.keys(opts).length == 0) return rv
+    if (opts.filter) rv = rv.filter(opts.filter)
+    if (opts.delete) rv = rv.forEach(e => e.remove())
+    return rv
+  },
+
+  // Add Global Event Listener that executes only on matching elements
+  agel(type, selector, callback, options, parent = document) {
+    parent.addEventListener(
+      type,
+      e => {
+        if (e.target.matches(selector)) callback(e)
+      },
+      options
+    )
   }
 }
 
---
+++ wapo.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        WPost Test
+// @namespace   Violentmonkey Scripts
+// @match       https://www.washingtonpost.com/*
+// @grant       none
+// @version     1.1
+// @require     https://insight-zen.github.io/monkey/base0.js
+// @author      -
+// @description 6/8/2022, 2:17:16 PM
+// ==/UserScript==
+
+const wp = {
+  unAnnoy(){
+    // D.qsa("div").filter(e => e.id.match(/^(pay|soft)wall/) ).forEach(e => e.remove() )
+    D.qsa("div", {filter: (e) => e.id.match(/^(pay|soft)wall/), delete: true})
+    h.style = ""
+    b.style = ""
+  }
+}
+
+const sBtn = "<button id='b101' style='position: fixed; padding: 0.5em; background-color: #fbbfbf; color: #232323; z-index: 10;'>UnAnnoy</button>"
+b.insertAdjacentHTML("afterbegin", sBtn)
+b.querySelector("#b101").addEventListener("click", wp.unAnnoy)
+
+D.purge()
+window.wp = wp
